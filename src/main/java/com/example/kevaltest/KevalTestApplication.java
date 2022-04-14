74c18422f6cb10b15994cfd74638caddda6f5e5e
Keval Server <keval.soni1990@gmail.com>
2022-04-14 15:01:36 +0530

    first commit

---
+++ src/main/java/com/example/kevaltest/KevalTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.kevaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KevalTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/kevaltest/MyController.java
@@ -0,0 +1,12 @@
+package com.example.kevaltest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping("/welcome-app")
+    public String getWelcome(){
+        return "My first Application updated";
+    }
+}
